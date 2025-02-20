45807a91b0330242f17cf34c52ccb061afd483de
Rob <robgreen490@gmail.com>
2025-02-20 15:58:40 -0600

    Modified Spring boot REST app with new method Step 6 in section 12

--- _Projects/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/e_commerce/spring_boot/dao/ProductRepository.java
+++ _Projects/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/e_commerce/spring_boot/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.e_commerce.spring_boot.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.e_commerce.spring_boot.entity.Product;
@@ -8,5 +11,6 @@ import com.e_commerce.spring_boot.entity.Product;
 // This allows our angular app to communicate to this port at 8080
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+    // Behind the scenes, Spring will execute a query similar to: SELECT * FROM product WHERE category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
