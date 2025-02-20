8700779200c00672d634da2c1f1201a64d88ab93
Rob <robgreen490@gmail.com>
2025-02-20 15:28:35 -0600

    Defined Routes

--- _Projects/ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ _Projects/ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { provideHttpClient } from '@angular/common/http'
 import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+
+// defining routes with an array of route items
+const routes: Routes =[
+  // The order of the routes are important, first match wins top down. Start most specific to most generic.
+  {path: 'category/id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},// this path redirects the user to the products page if no path is entered
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}// this path will match on anything that didn't match the above routes
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +24,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),// this points to the const routes
     BrowserModule,
     AppRoutingModule // AppRoutingModule replaced HttpClientModel
   ],
