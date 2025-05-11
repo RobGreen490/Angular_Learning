df43f25c3ca4561eb9bd937fd2673b96a3fedf3e
Robert <rgre552@wgu.edu>
2025-05-10 19:49:21 -0500

    Updated comments

--- Spring_Tutorials/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/myFirstSpringApp/myCoolApp/MyCoolAppApplication.java
+++ Spring_Tutorials/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/myFirstSpringApp/myCoolApp/MyCoolAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyCoolAppApplication {
 
-	// exposing '/' endpoint that returns "Hello World!"
 	public static void main(String[] args) {
 		SpringApplication.run(MyCoolAppApplication.class, args);
+		System.out.println("App Started..");
 	}
 }
--- Spring_Tutorials/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/myFirstSpringApp/myCoolApp/rest/FunRestController.java
+++ Spring_Tutorials/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/myFirstSpringApp/myCoolApp/rest/FunRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class FunRestController {
 
+    // exposing '/' endpoint that returns "Hello World!"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
